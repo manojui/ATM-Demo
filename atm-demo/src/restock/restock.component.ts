dda94acd7b87c888f54851a9c3aa5b7f99c90ffb
Manoj Gattu <manojbimi11@gmail.com>
2023-04-05 15:22:01 -0500

    Overview, withdrawl , restock components created

---
+++ atm-demo/src/restock/restock.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restock',
+  templateUrl: './restock.component.html',
+  styleUrls: ['./restock.component.sass']
+})
+export class RestockComponent {
+
+}
---
+++ atm-demo/src/withdrawl/withdrawl.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-withdrawl',
+  templateUrl: './withdrawl.component.html',
+  styleUrls: ['./withdrawl.component.sass']
+})
+export class WithdrawlComponent {
+
+}
